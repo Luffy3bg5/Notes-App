21a8c4930f9a3d380ca91f029961c0660eacebe5
Rohan <luffy7744.op@gmail.com>
2026-05-10 19:48:53 +0530

    added  the  feature  of  adding the  note  into  the  collection  array

---
+++ index.js
@@ -0,0 +1,38 @@
+let  collection = [] ;
+
+document.querySelector(".add-button").addEventListener('click' , add_note) ;
+
+// document.querySelector(".title").addEventlistener("click" , (e)=>{
+//     let  temp = {} ;
+    
+// })
+document.querySelector("")
+function add_note(){
+// get  title  value 
+let  inp = document.querySelector(".title-input").value ;
+
+
+// get  content  value 
+let  c = document.querySelector(".main-text-input").value ; 
+
+// create  object  
+let  temp = {
+    title :"",
+    content :""
+} ;
+
+// add  it  into  the  collection 
+if(inp !== ""){
+   temp.title = inp ;
+}
+else {
+    throw new Error("Title cannot  be blank");
+}
+
+temp.content = c ;
+
+collection.push(temp) ;
+
+console.log(collection) ;
+//console.log(collection )
+}
